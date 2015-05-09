992829bf16432c21b800f5a0b6402179d333c589
Saman <mohamadi.saman@gmail.com>
2015-05-09 12:53:30 +0430

    initial instance of Leveling

--- app/src/main/java/jvocab/jixa/com/jvocab/Leveling.java
+++ app/src/main/java/jvocab/jixa/com/jvocab/Leveling.java
@@ -12,7 +12,10 @@ import jvocab.jixa.com.jvocab.Model.Word;
 public class Leveling {
     DatabaseManager dbm;
     Leveling LVLInstance;
-    int[] factors;
+    // True Factor
+    int[] TF;
+    // False Factor
+    int[] FF;
 
     public Leveling getInstance() {
         if (null == LVLInstance)
@@ -22,7 +25,6 @@ public class Leveling {
 
     public List<Word> getWords(Context context) {
         int interval = 3000;
-        factors = new int[25];
         List<Word> words = new ArrayList<>();
         int t = 0;
         int sum = 0;
@@ -31,27 +33,28 @@ public class Leveling {
             for (int j = 0; j < 5; j++) {
                 int rnd = i + (int) (Math.random() * ((interval) + 1));
                 words.add(dbm.getWordById(context, rnd));
-                factors[t++] = rnd;
+                TF[t] = rnd;
+                FF[t++] = 10000 - rnd;
                 sum += rnd;
             }
             interval -= 500;
         }
         for (int i = 0; i < 25; i++) {
-            factors[i] = factors[i] / sum;
+            TF[i] = TF[i] * (10000 / sum);
+            FF[i] = (int) (FF[i] * (10000 / (25 * 10000 - sum)) * 0.5);
         }
         return words;
     }
 
-//    public int calculateLevel(boolean[] answers){
-//        int result = 0;
-//        float coefficient = 0.5f;
-//        for (int i=0 ; i<=25 ; i++){
-//            if (answers[i]){
-//                result += factors[i] * (1.4 - coefficient);
-//            }else{
-//                result += factors[]
-//            }
-//        }
-//        return 0;
-//    }
+    public int calculateLevel(boolean[] answers) {
+        int result = 0;
+        for (int i = 0; i <= 25; i++) {
+            if (answers[i]) {
+                result += TF[i] ;
+            } else {
+                result -= FF[i] ;
+            }
+        }
+        return result;
+    }
 }
